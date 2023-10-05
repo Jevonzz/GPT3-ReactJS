43f28e5e09c712cf176f893f9a94bef6ff4d147e
Keman <keatvun191@gmail.com>
2023-10-05 18:29:05 +0800

    Completed Feature and whatGPT3. Next Features

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,37 @@
 import React from "react";
-
+import Feature from "../../components/feature/Feature";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature
+          title="What is GPT3?"
+          text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi numquam maxime at cumque perspiciatis, ratione eveniet quas et voluptatum consequuntur?"
+        />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore the library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature
+          title="Chatbots"
+          text="Lorem ipsum dolor, sumquam maxime at cumque perspiciatis, ratione eveniet quas et voluptatum consequuntur?"
+        />
+        <Feature
+          title="Knowledgebase"
+          text="Lorem ipsum dolor, se at cumque perspiciatis, ratione eveniet quas et voluptatum consequuntur?"
+        />
+        <Feature
+          title="Education"
+          text="Lorem ipsum dolor, veniet quas et voluptatum consequuntur?"
+        />
+      </div>
+    </div>
   );
 };
 
